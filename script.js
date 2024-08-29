4edf85dda77fe95ca16d8202f1fc9f47293b61bb
gon_14 <poniemangon@gmail.com>
2024-08-29 15:22:17 -0300

    fixes

--- script.js
+++ script.js
@@ -1,19 +1,70 @@
 $(document).ready(function() {
-    $(window).scroll(function() {
-        if ($(this).scrollTop() > 50) { 
+
+    function toggleNavbarBackground() {
+        if ($('.mobile-menu').css('display') !== 'none') {
             $('.navbar-section').addClass('scrolled');
         } else {
-            $('.navbar-section').removeClass('scrolled');
+            $(window).trigger('scroll');
         }
+    }
+
+    $(window).scroll(function() {
+        if ($('.mobile-menu').css('display') === 'none') { 
+            if ($(this).scrollTop() > 50) {
+                $('.navbar-section').addClass('scrolled');
+            } else {
+                $('.navbar-section').removeClass('scrolled');
+            }
+        }
+    });
+
+    // Function to paint the background before toggling the menu
+    function paintAndToggleMenu() {
+        $('.navbar-section').addClass('scrolled');
+        $('.mobile-menu').slideToggle(toggleNavbarBackground);
+    }
+
+    // Bind the paintAndToggleMenu function to the burger menu icons
+    $('.fa-bars').click(function() {
+        paintAndToggleMenu();
+    });
+
+    $('.fa-x').click(function() {
+        paintAndToggleMenu();
     });
 });
 
-$(document).ready(function() {
-    $('.fa-bars').click(function() {
-        $('.mobile-menu').slideToggle();
+$('.carousel-banner').slick({
+    dots: true,
+    arrows: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    initialSlide: 1,
+  });
+
+
+  $(document).ready(function() {
+    // Toggle the download-modal inside the respective file div
+    $('.files-section .container .files .file .elypsis').on('click', function(event) {
+        event.stopPropagation(); // Prevent click event from bubbling up
+
+        // Close all other download-modal divs
+        $('.download-modal').not($(this).closest('.file').find('.download-modal')).hide();
+
+        // Toggle the current download-modal
+        $(this).closest('.file').find('.download-modal').toggle();
     });
-    
-    $('.fa-x').click(function() {
-        $('.mobile-menu').slideToggle();
+
+    // Close all download-modal divs if clicked anywhere else
+    $(document).on('click', function() {
+        $('.download-modal').hide();
+    });
+
+    // Prevent closing the modal when clicking inside the download-modal
+    $('.files-section .container .files .file .download-modal').on('click', function(event) {
+        event.stopPropagation();
     });
 });
+
